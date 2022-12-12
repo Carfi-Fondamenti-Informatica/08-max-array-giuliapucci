66c928e3286ae33bac42e04a6c077e017d69ad5d
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-12-12 14:58:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
+using namespace std;
+float x=0;
 int main(){
+  int n=0; cin >> n;
+    float numeri[n];
+    for (int i=0; i<n; i++){
+     cin >> numeri[i];
+    }
+    for (int i=0; i<n; i++){
+        float m = maggiore(numeri[i], numeri[i-1]);
+        if (m>x){
+            x=m;
+        }
+    }
+    cout << x;
   return 0;
 }
